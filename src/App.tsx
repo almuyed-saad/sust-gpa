8da99e31d37b554379c8016cb33f244ca3bcf545
almuyed-saad <iamsaad236@gmail.com>
2026-03-31 01:41:25 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Dashboard from "@/pages/Dashboard";
 
@@ -18,7 +17,7 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
+    
         <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
           <Router />
         </WouterRouter>
